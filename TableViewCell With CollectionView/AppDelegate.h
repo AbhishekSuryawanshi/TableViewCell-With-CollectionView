1dee6beea7a355ae9c1f90788aee304682913fea
Abhishek Suryawanshi <aysuryawanshi@gmail.com>
2018-04-27 11:07:16 +0530

    Initial Commit

---
+++ TableViewCell With CollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewCell With CollectionView
+//
+//  Created by Abhishek Suryawanshi on 27/04/18.
+//  Copyright © 2018 Abhishek Suryawanshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewCell With CollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewCell With CollectionView
+//
+//  Created by Abhishek Suryawanshi on 27/04/18.
+//  Copyright © 2018 Abhishek Suryawanshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
